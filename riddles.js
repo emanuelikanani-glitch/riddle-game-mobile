1f047a7e322bb51fc42688307ad953e8b44960c4
emanuelikanani-glitch <emanuelikanani@gmail.com>
2026-02-12 00:56:51 +0100

    Creating riddles.js with 5 progressive riddles and hints

---
+++ riddles.js
@@ -0,0 +1,24 @@
+const riddles = [
+    {
+        riddle: "What has keys but can't open locks?",
+        hint: "You use it to play music."
+    },
+    {
+        riddle: "What has to be broken before you can use it?",
+        hint: "It's often eaten."
+    },
+    {
+        riddle: "I’m tall when I’m young, and I’m short when I’m old. What am I?",
+        hint: "You often see me at a birthday party."
+    },
+    {
+        riddle: "I speak without a mouth and hear without ears. I have no body, but I come alive with the wind. What am I?",
+        hint: "I can be heard but not seen."
+    },
+    {
+        riddle: "The more of this there is, the less you see. What is it?",
+        hint: "It can make things disappear."
+    }
+];
+
+module.exports = riddles;
